0c9ce94fb50f88bd33eaa706348a33c6b2bc2297
kzk215 <jazzycat.aoiro@gmail.com>
2024-05-15 22:44:34 +0900

    gtm

--- fukuboku/inc/head.php
+++ fukuboku/inc/head.php
@@ -34,9 +34,20 @@ global $site_url;
   <!-- JS Library -->
   <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5KPPDKBJ');</script>
+  <!-- End Google Tag Manager -->
   <?php wp_head()?>
 
 </head>
 
 <body id="pageTop">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KPPDKBJ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div class="wrap">
